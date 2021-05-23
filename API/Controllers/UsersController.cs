ece05e14b2548e282a0fe4bfa091298eb68d955c
abhishek <abhishek.mis97@gmail.com>
2021-05-23 13:12:07 +0530

    Init

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Data;
+using System.Collections.Generic;
+using Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            return await _context.Users.FindAsync(id);
+        }
+
+    }
+}
