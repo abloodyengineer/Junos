8d3c5aa95591e6ac9651c16e071329c1739aefb0
abhishek <abhishek.mis97@gmail.com>
2021-08-02 19:58:45 +0530

    heroku

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using API.SignalR;
+using System;
 
 namespace API.Extensions
 {
